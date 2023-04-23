39f275520aa14570308cb38d8c5802b0de4962b1
Paulo Jerônimo <paulojeronimo@gmail.com>
2023-04-23 16:07:27 -0300

    Updated tmp-history at 2023-04-23T16:07:26-03:00

--- tmp-history/update-history.sh
+++ tmp-history/update-history.sh
@@ -4,11 +4,12 @@ set -o pipefail
 [ "$1" ] || { echo A date is required!; exit 1; }
 
 history_dir=${history_repo:-../paulojeronimo.github.io/tmp-history}
+changes_file="$history_dir"/changes.txt
 cd "$(dirname "$0")"
 
 declare -A history_files
 cd "$history_dir"
-for f in $(find . -type f ! -name files.txt ); do history_files[$f]=1; done
+for f in $(find . -type f ! -name $(basename "$changes_file")); do history_files[$f]=1; done
 cd "$OLDPWD"
 
 filter() {
@@ -19,7 +20,11 @@ filter() {
 }
 
 for f in ${!history_files[@]}; do cp "$f" "$history_dir"/; done
-find . -type f ! -path */.git/* | filter | sort | xargs sha256sum > "$history_dir"/files.txt
+
+echo -e "Update date/time: $1\n\nChanges:" > "$changes_file"
+git status --short >> "$changes_file"
+echo -e "\nTemporary files hashes:" >> "$changes_file"
+find . -type f ! -path */.git/* | filter | sort | xargs sha256sum >> "$changes_file"
 
 cd "$history_dir"
 git add .
